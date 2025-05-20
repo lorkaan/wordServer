19c7d2653822316b73d44df871c90060912981b9
Laurkan <laurkanr@gmail.com>
2025-05-19 20:24:21 -0800

    mistakenly deleted the include

--- wordblox/wordblox/urls.py
+++ wordblox/wordblox/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from wordtag.views import TagViewSet, WordViewSet, SpellinBloxPullHandler
 from utils.tokens import get_csrf_token
